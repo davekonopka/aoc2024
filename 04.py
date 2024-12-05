391c73694654e471e3faf49b0576084481621168
Dave Konopka <dave.konopka@gmail.com>
2024-12-05 07:53:02 -0500

    feat: add matrix visualizer, fix boundary offsets

--- 04.py
+++ 04.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import copy
 import re
 import sys
 
@@ -32,36 +33,102 @@ def getWord(matrix, x, y, direction, length):
     return "".join([matrix[y+i][x+i] for i in range(0, length)])
   return ""
 
+def checkMatrixPoint(matrix, x, y, length):
+  words = ["XMAS"]
+  # up
+  if y >= 3:
+    if "".join([matrix[y-i][x]["char"] for i in range(0, length)]) in words:
+      for i in range(0, length):
+        matrix[y-i][x]["hit"] = True
+  # down
+  if y + 4 <= len(matrix):
+    if "".join([matrix[y+i][x]["char"] for i in range(0, length)]) in words:
+      for i in range(0, length):
+        matrix[y+i][x]["hit"] = True
+  # left
+  if x >= 3:
+    if "".join([matrix[y][x-i]["char"] for i in range(0, length)]) in words:
+      for i in range(0, length):
+        matrix[y][x-i]["hit"] = True
+    # up-left
+    if y >= 3:
+      if y == 9 and x == 3:
+        print("".join([matrix[y-i][x-i]["char"] for i in range(0, length)]))
+      if "".join([matrix[y-i][x-i]["char"] for i in range(0, length)]) in words:
+        for i in range(0, length):
+          matrix[y-i][x-i]["hit"] = True
+    # down-left
+    if y + 4 <= len(matrix):
+      if "".join([matrix[y+i][x-i]["char"] for i in range(0, length)]) in words:
+        for i in range(0, length):
+          matrix[y+i][x-i]["hit"] = True
+  # right
+  if x + 4 <= len(matrix[y]):
+    if "".join([matrix[y][x+i]["char"] for i in range(0, length)]) in words:
+      for i in range(0, length):
+        matrix[y][x+i]["hit"] = True
+    # up-right
+    if y >= 3:
+      if "".join([matrix[y-i][x+i]["char"] for i in range(0, length)]) in words:
+        for i in range(0, length):
+          matrix[y-i][x+i]["hit"] = True
+    # down-right
+    if y + 4 <= len(matrix):
+      if "".join([matrix[y+i][x+i]["char"] for i in range(0, length)]) in words:
+        for i in range(0, length):
+          matrix[y+i][x+i]["hit"] = True
+  return matrix
+
+def printMatrix(matrix):
+  matrixCopy = copy.deepcopy(matrix)
+
+  # Build visual matrix
+  for y in range(0, len(matrixCopy)):
+    for x in range(0, len(matrixCopy[y])):
+      matrixCopy[y][x] = { "char": matrixCopy[y][x], "hit": False }
+
+  for y in range(0, len(matrixCopy)):
+    for x in range(0, len(matrixCopy[y])):
+      processedMatrix = checkMatrixPoint(matrixCopy, x, y, 4)
+
+  for y in range(0, len(matrixCopy)):
+    for x in range(0, len(matrixCopy[y])):
+      if matrixCopy[y][x]["hit"]:
+        color = "31m"
+      else:
+        color = "32m"
+      print(f"\033[{color}{matrixCopy[y][x]["char"]}\033[0m", end="")
+    print()
+
 matrix = [list(line) for line in data.strip().split("\n")]
 
+# Count the number of times the word "XMAS" appears in the matrix
 count = 0
-words = ["XMAS", "SAMX"]
+words = ["XMAS"]
 for y in range(0, len(matrix)):
   for x in range(0, len(matrix[y])):
     directions = []
-    # if x >= 4:
-      # directions.append("left")
-      # if y >= 4:
-      #   directions.append("up-left")
-      # if y <= len(matrix) - 4:
-      #   directions.append("down-left")
+    if x >= 3:
+      directions.append("left")
+      if y >= 3:
+        directions.append("up-left")
+      if y + 4 <= len(matrix):
+        directions.append("down-left")
     if x + 4 <= len(matrix[0]):
       directions.append("right")
-      if y >= 4:
+      if y >= 3:
         directions.append("up-right")
       if y + 4 <= len(matrix):
         directions.append("down-right")
-    # if y >= 4:
-    #   directions.append("up")
+    if y >= 3:
+      directions.append("up")
     if y + 4 <= len(matrix):
       directions.append("down")
 
     for direction in directions:
       word = getWord(matrix, x, y, direction, 4)
       if word in words:
-        print(f"word: {word}")
         count += 1
-      else:
-        print(f"*** word: {word}")
 
+printMatrix(matrix)
 print(f"count: {count}")
