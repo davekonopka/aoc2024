043672273c19f970162dfd3bebb314a398354d9a
Dave Konopka <dave.konopka@gmail.com>
2024-12-08 17:17:02 -0500

    feat: day 8 part 2

--- 08.py
+++ 08.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import math
+import copy
 import sys
 
 # https://adventofcode.com/2024/day/8/input
@@ -31,6 +31,8 @@ for y, line in enumerate(data.split("\n")):
           antenae[point] = []
         antenae[point].append((x, y))
 
+mapTwo = copy.deepcopy(map)
+
 antinodes = set()
 
 for signal in antenae:
@@ -42,7 +44,7 @@ for signal in antenae:
       xDist = ptOne[0] - ptTwo[0]
       antOne = (ptOne[0] + xDist, ptOne[1] + yDist)
       antTwo = (ptTwo[0] - xDist, ptTwo[1] - yDist)
-      print(f"Signal {signal} Points {ptOne} {ptTwo} xdist {xDist} ydist {yDist} Antinodes {antOne} {antTwo} MapHeight {mapHeight} MapWidth {mapWidth}")
+      # print(f"Signal {signal} Points {ptOne} {ptTwo} xdist {xDist} ydist {yDist} Antinodes {antOne} {antTwo} MapHeight {mapHeight} MapWidth {mapWidth}")
       if antOne[0] >= 0 and antOne[1] >= 0 and antOne[0] < mapWidth and antOne[1] < mapHeight - 1:
         antinodes.add(antOne)
         map[antOne[1]][antOne[0]] = "#"
@@ -56,4 +58,41 @@ for line in map:
     print(point, end="")
   print("")
 
-print(f"Antinode count: {len(antinodes)}")
+
+antinodesTwo = set()
+for signal in antenae:
+  for i, hit in enumerate(antenae[signal]):
+    for j in range(i+1, len(antenae[signal])):
+      ptOne = antenae[signal][i]
+      ptTwo = antenae[signal][j]
+      antinodesTwo.add(ptOne)
+      antinodesTwo.add(ptTwo)
+      yDist = ptOne[1] - ptTwo[1]
+      xDist = ptOne[0] - ptTwo[0]
+
+      while True:
+        antOne = (ptOne[0] + xDist, ptOne[1] + yDist)
+        if antOne[0] >= 0 and antOne[1] >= 0 and antOne[0] < mapWidth and antOne[1] < mapHeight - 1:
+          antinodesTwo.add(antOne)
+          mapTwo[antOne[1]][antOne[0]] = "#"
+          ptOne = antOne
+        else:
+          break
+
+      while True:
+        antTwo = (ptTwo[0] - xDist, ptTwo[1] - yDist)
+        if antTwo[0] >= 0 and antTwo[1] >= 0 and antTwo[0] < mapWidth and antTwo[1] < mapHeight - 1:
+          antinodesTwo.add(antTwo)
+          mapTwo[antTwo[1]][antTwo[0]] = "#"
+          ptTwo = antTwo
+        else:
+          break
+
+# Print map with antinodes
+for line in mapTwo:
+  for point in line:
+    print(point, end="")
+  print("")
+
+print(f"Part one antinode count: {len(antinodes)}")
+print(f"Part two antinode count: {len(antinodesTwo)}")
